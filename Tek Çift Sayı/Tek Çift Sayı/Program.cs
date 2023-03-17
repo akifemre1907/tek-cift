f8267460c92c6333a029f31526b02966a4ae6059
akifemre1907 <akifemre81@gmail.com>
2023-03-17 19:55:47 +0300

    tek-cift-sayi

---
+++ Tek Çift Sayı/Tek Çift Sayı/Program.cs
@@ -0,0 +1,14 @@
+﻿double sayi;
+
+Console.Write("Bir Sayı Giriniz: ");
+
+sayi = Convert.ToDouble(Console.ReadLine());
+
+if (sayi %2 == 0)
+{
+    Console.WriteLine("Girdiğiniz Sayı Çifttir.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz Sayı Tektir");
+}
